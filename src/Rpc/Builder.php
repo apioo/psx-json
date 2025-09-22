1892e2c37acfcf3aa605d312e6acc0073e107df7
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-09-22 23:16:18 +0200

    allow int and string as id

--- src/Rpc/Builder.php
+++ src/Rpc/Builder.php
@@ -31,7 +31,7 @@ class Builder
 {
     private const VERSION = '2.0';
 
-    public function createCall(string $method, mixed $arguments, int $id = null): object
+    public function createCall(string $method, mixed $arguments, int|string|null $id = null): object
     {
         return (object) [
             'jsonrpc' => self::VERSION,
@@ -41,7 +41,7 @@ class Builder
         ];
     }
 
-    public function createResponse(mixed $result, int $id = null): object
+    public function createResponse(mixed $result, int|string|null $id = null): object
     {
         return (object) [
             'jsonrpc' => self::VERSION,
@@ -50,7 +50,7 @@ class Builder
         ];
     }
 
-    public function createError(\Throwable $e, int $id = null, bool $debug = false): object
+    public function createError(\Throwable $e, int|string|null $id = null, bool $debug = false): object
     {
         $error = (object) [
             'code' => $e->getCode(),
